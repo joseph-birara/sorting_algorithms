f6dc41e1c7bff9069550d8bc579afca87a27206a
joseph-birara <josephbirara92@gmail.com>
2022-08-16 18:27:30 +0300

    attempt all mandatory tasks

--- sort.h
+++ sort.h
@@ -34,5 +34,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void heapify(int *array, int end, int start, size_t size);
 
 #endif
